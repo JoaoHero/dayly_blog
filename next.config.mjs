df3a1ac0ad4ce997338a5bcfeebeb0d1e0839c20
João Ramos <Joao.silvaramos2013@hotmail.com>
2024-06-05 07:44:07 -0300

    fix: removed domains that will no longer be used

--- next.config.mjs
+++ next.config.mjs
@@ -2,7 +2,7 @@
 const nextConfig = {
     reactStrictMode: true,
     images: {
-      domains: ["iuricode-blog.vercel.app", "img.freepik.com", "i.postimg.cc"]
+      domains: ["i.postimg.cc", "avatars.githubusercontent.com"]
     }
 };
 
