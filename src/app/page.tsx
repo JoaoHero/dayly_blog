c9b630155ec8f3bcc31e8ecc55ca45eb487a3182
João Ramos <Joao.silvaramos2013@hotmail.com>
2024-06-05 04:20:19 -0300

    fix: added header tag for respect semantic in site

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,8 +2,15 @@ import Navbar from "./components/Navbar";
 
 export default function Home() {
   return (
-    <main className="max-w-[800px] mx-auto">
-      <Navbar />
-    </main>
+    <>
+      <header className="max-w-[800px] mx-auto">
+        <Navbar />
+      </header>
+
+      <main className="max-w-[800px] mx-auto">
+        
+      </main>
+    </>
+
   );
 }
