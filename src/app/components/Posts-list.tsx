e7ca654d5ad462f22e4e9715f083158af7809ac4
João Ramos <Joao.silvaramos2013@hotmail.com>
2024-06-06 23:39:16 -0300

    fix: added key props giving id

--- src/app/components/Posts-list.tsx
+++ src/app/components/Posts-list.tsx
@@ -13,24 +13,11 @@ export default async function PostsList() {
         }
     })
 
-    // console.log(posts[1].likes.length)
-
-    // const likes = await db.like.findUnique({
-    //     where: {
-    //         id: posts[1].id
-    //     }
-    // })
-
-    // console.log(likes)
-
-    // console.log(posts[1].likes.length)
-    // const likeId: string = posts[1].likes[0].id
-
     return (
         <div className="flex flex-col gap-5 p-10">
             {posts.map((post) => (
                 <>
-                    <PostsItem id={post.id} title={post.title} shortDescription={post.shortDescription} tag={post.tag} />
+                    <PostsItem key={post.id} id={post.id} title={post.title} shortDescription={post.shortDescription} tag={post.tag} />
                 </>
             ))}
         </div>
