0713e96f329f896990d3fd414ff24d53b2334f00
João Ramos <Joao.silvaramos2013@hotmail.com>
2024-06-07 12:22:31 -0300

    added: gitHub domain

--- next.config.mjs
+++ next.config.mjs
@@ -2,7 +2,7 @@
 const nextConfig = {
     reactStrictMode: true,
     images: {
-      domains: ["i.postimg.cc", "avatars.githubusercontent.com"]
+      domains: ["i.postimg.cc", "avatars.githubusercontent.com", "github.com"]
     }
 };
 
