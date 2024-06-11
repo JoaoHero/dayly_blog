a3990ecc235c3eb72489c031aef7ab7c004f09ce
João Ramos <Joao.silvaramos2013@hotmail.com>
2024-06-11 00:54:51 -0300

    fix: change the name in link href

--- src/app/components/Posts-Item.tsx
+++ src/app/components/Posts-Item.tsx
@@ -11,7 +11,7 @@ interface PostsItemProps {
 export default function PostsItem({ id, title, shortDescription, tag }: PostsItemProps ) {
     return (
         <section className="max-w-[1200px] mx-auto flex flex-col gap-10 relative">
-          <Link href={`/blogPosts/${id}`} key={id}>
+          <Link href={`/posts/${id}`} key={id}>
               <div  className="w-full border-2 border-[--color-dark-400] bg-[--color-dark-200] hover:bg-[--color-dark-100] transition-colors duration-500 p-4">
                 <h2 className="text-xl text-white font-bold">{title}</h2>
                 <p className="text-[--color-white-200] mt-5 text-justify">{shortDescription}</p>
